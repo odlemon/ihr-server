e27f2b8b038266a937575d2c34c4923388325c57
Nyasha Karata <nyashakarata35@gmail.com>
2024-06-28 00:35:51 +0200

    save point

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -16,22 +16,23 @@ import { isAdminRoute, protectRoute } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create", protectRoute, createTask);
-router.post("/duplicate/:id", protectRoute, duplicateTask);
+router.post("/create", protectRoute, isAdminRoute, createTask);
+router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
 router.post("/activity/:id", protectRoute, postTaskActivity);
 
 router.get("/dashboard", protectRoute, dashboardStatistics);
 router.get("/", protectRoute, getTasks);
 router.get("/:id", protectRoute, getTask);
 
-router.put("/create-subtask/:id", protectRoute, createSubTask);
-router.put("/update/:id", protectRoute, updateTask);
+router.put("/create-subtask/:id", protectRoute, isAdminRoute, createSubTask);
+router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
 router.put("/change-stage/:id", protectRoute, updateTaskStage);
-router.put("/:id", protectRoute, trashTask);
+router.put("/:id", protectRoute, isAdminRoute, trashTask);
 
 router.delete(
   "/delete-restore/:id?",
   protectRoute,
+  isAdminRoute,
   deleteRestoreTask
 );
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -19,7 +19,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
-router.get("/get-team", protectRoute, getTeamList);
+router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
 router.get("/notifications", protectRoute, getNotificationsList);
 
 router.put("/profile", protectRoute, updateUserProfile);
@@ -28,7 +28,7 @@ router.put("/change-password", protectRoute, changeUserPassword);
 //   FOR ADMIN ONLY - ADMIN ROUTES
 router
   .route("/:id")
-  .put(protectRoute, activateUserProfile)
-  .delete(protectRoute, deleteUserProfile);
+  .put(protectRoute, isAdminRoute, activateUserProfile)
+  .delete(protectRoute, isAdminRoute, deleteUserProfile);
 
 export default router;

95c3162d3d1b038921de45b5fdd80e61a4bf26b3
Nyasha Karata <164152028+odlemon@users.noreply.github.com>
2024-06-29 08:14:23 +0200

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -7,8 +7,8 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: true, // Use secure cookies in production
+    sameSite: "none", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };

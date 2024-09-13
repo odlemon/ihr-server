17ab2f53842197fa82c8ec8574c7277e3b7d9492
Nyasha Karata <nyashakarata35@gmail.com>
2024-09-13 07:50:42 +0200

    updated CORS

--- server/index.js
+++ server/index.js
@@ -15,9 +15,21 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://www.ihractive.com', 
+];
+
 app.use(
   cors({
-    origin: true, 
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
